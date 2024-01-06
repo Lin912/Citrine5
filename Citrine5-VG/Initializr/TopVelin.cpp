8ab24f9c423112e2f31e2de4eed09a5c527aa302
Lyn <18340802816@163.com>
2024-01-06 23:24:34 +0800

    new file:   Citrine5-VG/CSV/Delta.csv 	new file:   Citrine5-VG/CSV/Nums.csv 	new file:   Citrine5-VG/CSV/Parameters.csv 	new file:   Citrine5-VG/CSV/TopVel.csv 	new file:   Citrine5-VG/CSV/Water.csv 	new file:   Citrine5-VG/Initializr/TopVelin.cpp 	new file:   Citrine5-VG/Initializr/Water.cpp 	new file:   Citrine5-VG/Initializr/a.exe

---
+++ Citrine5-VG/Initializr/TopVelin.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <fstream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+    int Nodes = 50;
+    int variable = 10;
+    int TV = 500;                                                //总变量数TV= Nodes * variable
+    int TimeStep = 2000;                                         //总时间步数
+    double DelTime = 0.005;                                      //时间步长(真实时间步长)
+    double pi = 3.1415926;
+
+    MatrixXd a(TimeStep, TV);
+    a.Zero(TimeStep, TV);
+    //*********************************************************************//
+    MatrixXd b(TimeStep, 3);
+    b.Zero(TimeStep, 3);
+    for(int i = 0; i < TimeStep; i++)
+    {
+        b(i, 0) = 0;
+        b(i, 1) = -0.3 * 2*pi/2.5 *sin(2*pi/2.5 * (i+1) * DelTime);
+        b(i, 2) = 0.3 * 2*pi/2.5 *cos(2*pi/2.5 * (i+1) * DelTime);
+    }
+
+    ofstream dataFile;
+    dataFile.open(".././csv./TopVel.csv", ios::out | ios::trunc);
+    for(int i = 0; i < TimeStep; i++)
+    {
+        for(int j = 0; j < 3; j++)
+        {
+            dataFile << b(i, j) << ",";
+        }
+        dataFile << endl;
+    }
+    dataFile.close();
+    return 0;
+}
