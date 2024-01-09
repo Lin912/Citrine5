89e3939be577c46200f540ab1041cc0ea2a4f166
Lyn <18340802816@163.com>
2024-01-09 15:07:42 +0800

    modified:   .vscode/settings.json 	new file:   Citrine5/CMakeLists.txt 	deleted:    Citrine5/txt/Complie.txt 	new file:   build/.cmake/api/v1/query/client-vscode/query.json 	new file:   build/.cmake/api/v1/reply/cache-v2-e67376327d5d1729e42d.json 	new file:   build/.cmake/api/v1/reply/cmakeFiles-v1-89481d2e46742c27c238.json 	new file:   build/.cmake/api/v1/reply/codemodel-v2-25a97dd50a356a07550e.json 	new file:   build/.cmake/api/v1/reply/directory-.-Debug-d0094a50bb2071803777.json 	new file:   build/.cmake/api/v1/reply/index-2024-01-09T07-01-25-0656.json 	new file:   build/.cmake/api/v1/reply/target-Citrine5-Debug-7820c31f1331754a794d.json 	new file:   build/.cmake/api/v1/reply/toolchains-v1-abe318a8b4f677a30a50.json 	new file:   build/CMakeCache.txt 	new file:   build/CMakeFiles/3.23.1/CMakeCCompiler.cmake 	new file:   build/CMakeFiles/3.23.1/CMakeCXXCompiler.cmake 	new file:   build/CMakeFiles/3.23.1/CMakeRCCompiler.cmake 	new file:   build/CMakeFiles/3.23.1/CMakeSystem.cmake 	new file:   build/CMakeFiles/3.23.1/CompilerIdC/CMakeCCompilerId.c 	new file:   build/CMakeFiles/3.23.1/CompilerIdC/a.exe 	new file:   build/CMakeFiles/3.23.1/CompilerIdCXX/CMakeCXXCompilerId.cpp 	new file:   build/CMakeFiles/3.23.1/CompilerIdCXX/a.exe 	new file:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	new file:   build/CMakeFiles/CMakeError.log 	new file:   build/CMakeFiles/CMakeOutput.log 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Add.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Add.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/BC.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/BC.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Fiber.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Fiber.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Fx.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Fx.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Iterator.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Iterator.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Jacobian.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Jacobian.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Load.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/Load.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/MNQ.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/MNQ.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/ReadOut.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/ReadOut.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/main.cpp.obj 	new file:   build/CMakeFiles/Citrine5.dir/Cpp/main.cpp.obj.d 	new file:   build/CMakeFiles/Citrine5.dir/DependInfo.cmake 	new file:   build/CMakeFiles/Citrine5.dir/build.make 	new file:   build/CMakeFiles/Citrine5.dir/cmake_clean.cmake 	new file:   build/CMakeFiles/Citrine5.dir/compiler_depend.internal 	new file:   build/CMakeFiles/Citrine5.dir/compiler_depend.make 	new file:   build/CMakeFiles/Citrine5.dir/compiler_depend.ts 	new file:   build/CMakeFiles/Citrine5.dir/depend.make 	new file:   build/CMakeFiles/Citrine5.dir/flags.make 	new file:   build/CMakeFiles/Citrine5.dir/includes_CXX.rsp 	new file:   build/CMakeFiles/Citrine5.dir/link.txt 	new file:   build/CMakeFiles/Citrine5.dir/linklibs.rsp 	new file:   build/CMakeFiles/Citrine5.dir/objects.a 	new file:   build/CMakeFiles/Citrine5.dir/objects1.rsp 	new file:   build/CMakeFiles/Citrine5.dir/progress.make 	new file:   build/CMakeFiles/Makefile.cmake 	new file:   build/CMakeFiles/Makefile2 	new file:   build/CMakeFiles/TargetDirectories.txt 	new file:   build/CMakeFiles/cmake.check_cache 	new file:   build/CMakeFiles/progress.marks 	new file:   build/Citrine5.exe 	new file:   build/Makefile 	new file:   build/cmake_install.cmake 	new file:   build/compile_commands.json

---
+++ build/CMakeFiles/Citrine5.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Git/Citrine5/Citrine5/Cpp/Add.cpp" "CMakeFiles/Citrine5.dir/Cpp/Add.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Add.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/BC.cpp" "CMakeFiles/Citrine5.dir/Cpp/BC.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/BC.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/Fiber.cpp" "CMakeFiles/Citrine5.dir/Cpp/Fiber.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Fiber.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/Fx.cpp" "CMakeFiles/Citrine5.dir/Cpp/Fx.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Fx.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/Iterator.cpp" "CMakeFiles/Citrine5.dir/Cpp/Iterator.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Iterator.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/Jacobian.cpp" "CMakeFiles/Citrine5.dir/Cpp/Jacobian.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Jacobian.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/Load.cpp" "CMakeFiles/Citrine5.dir/Cpp/Load.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/Load.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/MNQ.cpp" "CMakeFiles/Citrine5.dir/Cpp/MNQ.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/MNQ.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/ReadOut.cpp" "CMakeFiles/Citrine5.dir/Cpp/ReadOut.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/ReadOut.cpp.obj.d"
+  "E:/Git/Citrine5/Citrine5/Cpp/main.cpp" "CMakeFiles/Citrine5.dir/Cpp/main.cpp.obj" "gcc" "CMakeFiles/Citrine5.dir/Cpp/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
