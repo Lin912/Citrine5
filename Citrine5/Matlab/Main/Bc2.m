04401b0c753803c852cb4abe43e615b8ec81a285
Lin912 <18340802816@163.com>
2024-05-10 17:33:36 +0800

    modified:   Citrine5/Matlab/Main/Bc2.m

--- Citrine5/Matlab/Main/Bc2.m
+++ Citrine5/Matlab/Main/Bc2.m
@@ -6,17 +6,17 @@ syms rho Cdn Cdb Sdn Sdb;
 
 Trans = [cos(Theta)*cos(Phi),-sin(Phi),cos(Phi)*sin(Theta);cos(Theta)*sin(Phi),cos(Phi),sin(Phi)*sin(Theta);-sin(Theta),0,cos(Theta)];
 
-g = [Gz;Gx;Gy]
-t = [T;Sn;Sb]
+g = [Gz,Gx,Gy]
+t = [T,Sn,Sb]
 
-gg = Trans*g;
+gg = g*Trans;
 hdr1 = 0.5*rho*Cdn*Sdn*v*(v^2+w^2)^(1/2);
 hdr2 = 0.5*rho*Cdb*Sdb*w*(v^2+w^2)^(1/2);
 
 f = t - gg;
-% f(2) = f(2) + hdr1;
-% f(3) = f(3) + hdr2;
+f(2) = f(2) - hdr1;
+f(3) = f(3) - hdr2;
 f(4) = Omega2;
 f(5) = Omega3;
 
-jac = jacobian(f,[u,v,w,T,Sn,Sb,Theta,Phi,Omega2,Omega3])
+jac = jacobian(f.',[u,v,w,T,Sn,Sb,Theta,Phi,Omega2,Omega3])
